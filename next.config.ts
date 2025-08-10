1918082b423ddc66a545df907ad5aecb22cbe0a8
r4k3labs <r4k3labs@gmail.com>
2025-08-10 22:29:22 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  devIndicators: false,
+  experimental: {
+    viewTransition: true,
+  },
 };
 
 export default nextConfig;
