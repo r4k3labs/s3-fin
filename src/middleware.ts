fb16f75022ca38c220315995a6f1f6568102f558
r4k3labs <r4k3labs@gmail.com>
2025-08-11 09:37:55 +0530

    simple middleware

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { createAuthClient } from "better-auth/client";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+const client = createAuthClient();
+
+export async function middleware(request: NextRequest) {
+  const { data: session } = await client.getSession({
+    fetchOptions: {
+      headers: {
+        cookie: request.headers.get("cookie") || "",
+      },
+    },
+  });
+
+  if (!session) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/admin", "/home"],
+};
