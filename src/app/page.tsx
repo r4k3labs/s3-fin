10279a50299ffa19a8a8da2d7948b6faf59df09a
r4k3labs <r4k3labs@gmail.com>
2025-08-11 10:35:52 +0530

    fix link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,22 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+  const { data: session, isPending } = authClient.useSession();
+
+  useEffect(() => {
+    if (!session && !isPending) {
+      router.push("/sign-in");
+    }
+  }, [session, isPending]);
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="bg-primary text-primary-foreground">
       Simple Sass Starter
